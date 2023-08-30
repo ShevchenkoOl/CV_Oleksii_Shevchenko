975830b2824a489c1c8e36f908f2cc2c7cdb3290
Oleksii Shevchenko <uzlabini@gmail.com>
2023-08-30 23:14:29 +0200

    add download

--- js/lang.js
+++ js/lang.js
@@ -1,82 +1,132 @@
-const langArr = {
-    "name" :  {
-        "en": "Oleksii Shevchenko",
-        "cz": "Oleksii Shevchenko",
-        "ua": "Олексій Шевченко",
-    }, 
-    "summary": {
-        "en": "SUMMARY",
-        "cz": "PŘEHLED",
-        "ua": "КОРОТКИЙ ОПИС",
-    }, 
-    "summary-text": {
-        "en": "I am a junior Front-End developer, looking for a full-time job in an information technology company. I have good knowledge of HTML, CSS, JavaScript, as well as Node.js server framework and React library. In addition, I have practical experience of working in a team using the Agile/Scrum methodology. I am a quick learner, responsible and ready for difficult tasks. My motto in life is that there are no impossible tasks.",
-        "cz": "Jsem Junior Front-REnd Developer a hledám práci na plný úvazek ve společnosti informačních technologií. Mám dobré znalosti HTML, CSS, JavaScript, stejně jako serverový framework Node.js a knihovnu React. Navíc mám praktickou zkušenost s prací v týmu pomocí metodiky Agile/Scrum. Rychle se učím, jsem zodpovědný a připravený na těžké úkoly. Moje životní motto: nejsou úkoly, které by se nedaly splnit.",
-        "ua": "Я Junior Front-End Devoloper, шукаю роботу на повний робочий день в компанії інформаційних технологій. Я добре знаю HTML, CSS, JavaScript, а також структуру сервера Node.js і бібліотеку React. Крім того, маю практичний досвід роботи в команді за методологією Agile/Scrum. Швидко навчаюсь, відповідальний  та готовий до складних завдань. Мій життєвий девіз: немає завдань, які неможливо виконати.",
-    }, 
-    "project": {
-        "en": "PROJECT EXPERIENCE",
-        "cz": "PROJEKTOVÉ ZKUŠENOSTI",
-        "ua": "ДОСВІД В ПРОЕКТАХ",
-    }, 
-    "url": {
-        "en": "link",
-        "cz": "Odkaz",
-        "ua": "Посилання",
-    }, 
-    "webstudio": {
-        "en": "[HTML, SASS, JS], personal project. The website was created with a adaptive layout. The project was built on GitHub, checked for build errors and page performance in real time.",
-        "cz": "[HTML, SASS, JS], osobní projekt. Webové stránky byly vytvořeny s adaptivním rozložením. Projekt byl postaven na GitHub, kontrolován na chyby sestavení a výkon stránky v reálném čase.",
-        "ua": "[HTML, SASS, JS], персональний проект. Сайт створено з адаптивною версткою. Проект був побудований на GitHub, перевірено на помилки збірки та продуктивність сторінки в реальному часі.",
-    }, 
-    "iceream": {
-        "en": "[HTML, SASS, JS], team project, my role on the project is a developer. The website was created with a adaptive layout. The project was built on GitHub, When working, the SCRUM methodology was used, checked for build errors and page performance in real time.",
-        "cz": "[HTML, SASS, JS], týmový projekt, moje role na projektu je vývojář. Webové stránky byly vytvořeny s adaptivním rozložením. Projekt byl postaven na GitHub, při práci byla použita metodika SCRUM, kontrolovány chyby sestavení a výkon stránky v reálném čase.",
-        "ua": "[HTML, SASS, JS], командний проект, моя роль у проекті – розробник. Сайт створено з адаптивною версткою. Проект побудований на GitHub, при роботі використовувалася методологія SCRUM, перевірялася на помилки збірки і продуктивність сторінки в реальному часі.",
-    }, 
-    "filmoteka": {
-        "en": "[HTML, SASS, JS], team project, my role on the project is a developer. The website was created with a adaptive layout. The project was built on GitHub, using different libraries. When working, the SCRUM methodology was used, checked for build errors and page performance in real time.",
-        "cz": "[HTML, SASS, JS], týmový projekt, moje role na projektu je vývojář. Webové stránky byly vytvořeny s adaptivním rozložením. Projekt byl postaven na GitHub s použitím různých knihoven. Při práci byla použita metodika SCRUM, kontrolována na chyby sestavení a výkon stránky v reálném čase.",
-        "ua": "[HTML, SASS, JS], командний проект, моя роль у проекті – розробник. Сайт створено з адаптивною версткою. Проект був побудований на GitHub з використанням різних бібліотек. При роботі використовувалася методологія SCRUM, перевірено на помилки збірки та продуктивність сторінки в реальному часі.",
-    }, 
-    "work": {
-        "en": "WORK EXPERIENCE",
-        "cz": "PRACOVNÍ ZKUŠENOST",
-        "ua": "ДОСВІД РОБОТИ",
-    }, 
-    "work1": {
-        "en": "State border service of Ukraine",
-        "cz": "Státní pohraniční služba Ukrajiny",
-        "ua": "Державна прикордонна служба України",
-    }, 
-    "position": {
-        "en": "officer of the operational-search department",
-        "cz": "důstojník operačně-pátrání",
-        "ua": "співробітник оперативно-розшукового відділу",
-    }, 
-    "date1": {
-        "en": "June 2006 - October 2016 Odesa, Ukraine",
-        "cz": "Červen 2006 - Říjen 2016 Odesa, Ukrajina",
-        "ua": "Червень 2006 - Жовтень 2016 Одеса, Україна",
-    }, 
-    "position1": {
-        "en": "accommodation services;",
-        "cz": "ubytovací služby;",
-        "ua": "послуги розміщення;",
-    }, 
-    "position2": {
-        "en": "customer service;",
-        "cz": "služby zákazníkům;",
-        "ua": "обслуговування клієнтів;",
-    }, 
-    "position3": {
-        "en": "reservations manager.",
-        "cz": "správce rezervací.",
-        "ua": "менеджер з бронювання.",
-    }, 
-    "date2": {
-        "en": "April 2017 - to date Prague, Czech Republic",
-        "cz": "Duben 2017 - dodnes Praha, Česká republika",
-        "ua": "Квітень 2017 - до сьогодні Прага, Чехія",
-    }, 
-}
+// Получаем элементы, с которыми будем взаимодействовать
+const languageSelect = document.getElementById('languageSelect');
+
+// Определение переводов для разных языков
+const translations = {
+    en: {
+        title: 'JUNIOR FRONTEND DEVELOPER',
+        en: 'EN',
+        cz: 'CZ',
+        ua: 'UA',
+        // ... другие переводы
+    },
+    cz: {
+        title: 'JUNIOR FRONTEND DEVELOPER',
+        en: 'EN',
+        cz: 'CZ',
+        ua: 'UA',
+        // ... другие переводы
+    },
+    ua: {
+        title: 'МОЛОДШИЙ ФРОНТЕНД РОЗРОБНИК',
+        en: 'EN',
+        cz: 'CZ',
+        ua: 'UA',
+        // ... другие переводы
+    }
+    // ... другие языковые переводы
+};
+
+// Обработчик события изменения выбранного языка
+languageSelect.addEventListener('change', function() {
+    const selectedLanguage = languageSelect.value;
+    const translation = translations[selectedLanguage];
+
+    // Применяем переводы к элементам на странице
+    document.title = translation.title;
+    languageSelect.options[0].textContent = translation.en;
+    languageSelect.options[1].textContent = translation.cz;
+    languageSelect.options[2].textContent = translation.ua;
+    // ... обновляем переводы для остальных элементов
+
+    // Выполняем другие действия в зависимости от выбранного языка
+    if (selectedLanguage === 'en') {
+        // Действия для английского языка
+    } else if (selectedLanguage === 'cz') {
+        // Действия для чешского языка
+    } else if (selectedLanguage === 'ua') {
+        // Действия для украинского языка
+    }
+});
+// const langArr = {
+//     "name" :  {
+//         "en": "Oleksii Shevchenko",
+//         "cz": "Oleksii Shevchenko",
+//         "ua": "Олексій Шевченко",
+//     }, 
+//     "summary": {
+//         "en": "SUMMARY",
+//         "cz": "PŘEHLED",
+//         "ua": "КОРОТКИЙ ОПИС",
+//     }, 
+//     "summary-text": {
+//         "en": "I am a junior Front-End developer, looking for a full-time job in an information technology company. I have good knowledge of HTML, CSS, JavaScript, as well as Node.js server framework and React library. In addition, I have practical experience of working in a team using the Agile/Scrum methodology. I am a quick learner, responsible and ready for difficult tasks. My motto in life is that there are no impossible tasks.",
+//         "cz": "Jsem Junior Front-REnd Developer a hledám práci na plný úvazek ve společnosti informačních technologií. Mám dobré znalosti HTML, CSS, JavaScript, stejně jako serverový framework Node.js a knihovnu React. Navíc mám praktickou zkušenost s prací v týmu pomocí metodiky Agile/Scrum. Rychle se učím, jsem zodpovědný a připravený na těžké úkoly. Moje životní motto: nejsou úkoly, které by se nedaly splnit.",
+//         "ua": "Я Junior Front-End Devoloper, шукаю роботу на повний робочий день в компанії інформаційних технологій. Я добре знаю HTML, CSS, JavaScript, а також структуру сервера Node.js і бібліотеку React. Крім того, маю практичний досвід роботи в команді за методологією Agile/Scrum. Швидко навчаюсь, відповідальний  та готовий до складних завдань. Мій життєвий девіз: немає завдань, які неможливо виконати.",
+//     }, 
+//     "project": {
+//         "en": "PROJECT EXPERIENCE",
+//         "cz": "PROJEKTOVÉ ZKUŠENOSTI",
+//         "ua": "ДОСВІД В ПРОЕКТАХ",
+//     }, 
+//     "url": {
+//         "en": "link",
+//         "cz": "Odkaz",
+//         "ua": "Посилання",
+//     }, 
+//     "webstudio": {
+//         "en": "[HTML, SASS, JS], personal project. The website was created with a adaptive layout. The project was built on GitHub, checked for build errors and page performance in real time.",
+//         "cz": "[HTML, SASS, JS], osobní projekt. Webové stránky byly vytvořeny s adaptivním rozložením. Projekt byl postaven na GitHub, kontrolován na chyby sestavení a výkon stránky v reálném čase.",
+//         "ua": "[HTML, SASS, JS], персональний проект. Сайт створено з адаптивною версткою. Проект був побудований на GitHub, перевірено на помилки збірки та продуктивність сторінки в реальному часі.",
+//     }, 
+//     "iceream": {
+//         "en": "[HTML, SASS, JS], team project, my role on the project is a developer. The website was created with a adaptive layout. The project was built on GitHub, When working, the SCRUM methodology was used, checked for build errors and page performance in real time.",
+//         "cz": "[HTML, SASS, JS], týmový projekt, moje role na projektu je vývojář. Webové stránky byly vytvořeny s adaptivním rozložením. Projekt byl postaven na GitHub, při práci byla použita metodika SCRUM, kontrolovány chyby sestavení a výkon stránky v reálném čase.",
+//         "ua": "[HTML, SASS, JS], командний проект, моя роль у проекті – розробник. Сайт створено з адаптивною версткою. Проект побудований на GitHub, при роботі використовувалася методологія SCRUM, перевірялася на помилки збірки і продуктивність сторінки в реальному часі.",
+//     }, 
+//     "filmoteka": {
+//         "en": "[HTML, SASS, JS], team project, my role on the project is a developer. The website was created with a adaptive layout. The project was built on GitHub, using different libraries. When working, the SCRUM methodology was used, checked for build errors and page performance in real time.",
+//         "cz": "[HTML, SASS, JS], týmový projekt, moje role na projektu je vývojář. Webové stránky byly vytvořeny s adaptivním rozložením. Projekt byl postaven na GitHub s použitím různých knihoven. Při práci byla použita metodika SCRUM, kontrolována na chyby sestavení a výkon stránky v reálném čase.",
+//         "ua": "[HTML, SASS, JS], командний проект, моя роль у проекті – розробник. Сайт створено з адаптивною версткою. Проект був побудований на GitHub з використанням різних бібліотек. При роботі використовувалася методологія SCRUM, перевірено на помилки збірки та продуктивність сторінки в реальному часі.",
+//     }, 
+//     "work": {
+//         "en": "WORK EXPERIENCE",
+//         "cz": "PRACOVNÍ ZKUŠENOST",
+//         "ua": "ДОСВІД РОБОТИ",
+//     }, 
+//     "work1": {
+//         "en": "State border service of Ukraine",
+//         "cz": "Státní pohraniční služba Ukrajiny",
+//         "ua": "Державна прикордонна служба України",
+//     }, 
+//     "position": {
+//         "en": "officer of the operational-search department",
+//         "cz": "důstojník operačně-pátrání",
+//         "ua": "співробітник оперативно-розшукового відділу",
+//     }, 
+//     "date1": {
+//         "en": "June 2006 - October 2016 Odesa, Ukraine",
+//         "cz": "Červen 2006 - Říjen 2016 Odesa, Ukrajina",
+//         "ua": "Червень 2006 - Жовтень 2016 Одеса, Україна",
+//     }, 
+//     "position1": {
+//         "en": "accommodation services;",
+//         "cz": "ubytovací služby;",
+//         "ua": "послуги розміщення;",
+//     }, 
+//     "position2": {
+//         "en": "customer service;",
+//         "cz": "služby zákazníkům;",
+//         "ua": "обслуговування клієнтів;",
+//     }, 
+//     "position3": {
+//         "en": "reservations manager.",
+//         "cz": "správce rezervací.",
+//         "ua": "менеджер з бронювання.",
+//     }, 
+//     "date2": {
+//         "en": "April 2017 - to date Prague, Czech Republic",
+//         "cz": "Duben 2017 - dodnes Praha, Česká republika",
+//         "ua": "Квітень 2017 - до сьогодні Прага, Чехія",
+//     }, 
+// }
